9cd655c7bc50706575189aa45f6f48f570083cb6
chanu0073 <chanu6692@gmail.com>
2026-01-17 17:33:11 +0530

    Set up Express server with nodemon for development

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,1 +1,17 @@
-console.log("backend start");
+import dotenv from "dotenv"
+import express from "express"
+
+dotenv.config({
+    path: "./.env"
+});
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+    res.send("hello world");
+});
+
+app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+})
