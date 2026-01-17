715ce4b8cb479af9a167efde883dd0b02548d27c
chanu0073 <chanu6692@gmail.com>
2026-01-17 23:46:54 +0530

    Add JSON parser and static middleware to Express app

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,10 @@ import express from "express"
 
 const app = express();
 
+// basic configuration
+app.use(express.json({ limit: "16kb" }));
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
     res.send("hello world");
 });
