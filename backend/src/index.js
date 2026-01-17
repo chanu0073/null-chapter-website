9e460e75611a780a5bd9ae89fb3307b4d7f09d4a
chanu0073 <chanu6692@gmail.com>
2026-01-18 02:57:26 +0530

    Set up MongoDB Atlas Connection

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv"
 import app from "./app.js";
+import connectDB from "./db/connect.js";
 
 dotenv.config({
     path: "./.env"
@@ -7,6 +8,15 @@ dotenv.config({
 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => {
-    console.log(`App listening on port ${port}`);
-})
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`App listening on port ${port}`);
+        });
+    })
+    .catch((err) => {
+        console.error("MongoDB connection error", err);
+        process.exit(1);
+    })
+
+
