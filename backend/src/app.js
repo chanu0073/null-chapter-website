be85785749add3ba96b2177d07b576f853549efa
chanu0073 <chanu6692@gmail.com>
2026-01-18 00:37:28 +0530

    Configure CORS middleware in Express app

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,10 +1,16 @@
 import express from "express"
+import cors from "cors"
 
 const app = express();
 
 // basic configuration
 app.use(express.json({ limit: "16kb" }));
 app.use(express.static("public"));
+
+// CORS configuration
+app.use(cors({
+    origin: process.env.CORS_ORIGIN
+}));
 
 app.get("/", (req, res) => {
     res.send("hello world");
